90a19ba04762c0a79c4bafa9bfa8dd7ab2231ed1
Hoang Phuc <phuchoang.inf@gmail.com>
2018-06-04 16:02:54 +0700

    js-async-await

--- src/index.js
+++ src/index.js
@@ -1,51 +1,40 @@
-/* ES7 */
-const isMomHappy = true;
+const isMomHappy = true
 
-// Promise
-const willIGetNewPhone = new Promise(
-    (resolve, reject) => {
-        if (isMomHappy) {
-            const phone = {
-                brand: 'Samsung',
-                color: 'black'
-            };
-            resolve(phone);
-        } else {
-            const reason = new Error('mom is not happy');
-            reject(reason);
-        }
-
+const willGetNewPhone = new Promise((resolve, reject) => {
+  if (isMomHappy) {
+    const phone = {
+      brand: 'iPhone',
+      color: 'black',
     }
-);
+    resolve(phone)
+  } else {
+    const reason = new Error('mom is not happy')
+    reject(reason)
+  }
+})
 
-// 2nd promise
 async function showOff(phone) {
-    return new Promise(
-        (resolve, reject) => {
-            var message = 'Hey friend, I have a new ' +
-                phone.color + ' ' + phone.brand + ' phone';
+  const message = `Hey buddy, I have a new ${phone.color} ${
+    phone.brand
+  }!!!`
 
-            resolve(message);
-        }
-    );
-};
-
-// call our promise
-async function askMom() {
-    try {
-        console.log('before asking Mom');
-
-        let phone = await willIGetNewPhone;
-        let message = await showOff(phone);
-
-        console.log(message);
-        console.log('after asking mom');
-    }
-    catch (error) {
-        console.log(error.message);
-    }
+  return Promise.resolve(message)
 }
 
-(async () => {
-    await askMom();
-})();
+async function askMom() {
+  try {
+    console.log('before asking Mom')
+
+    let phone = await willGetNewPhone
+    let message = await showOff(phone)
+
+    console.log(message)
+    console.log('after asking Mom')
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+;(async () => {
+  await askMom()
+})()
